0b001e82a3ae00c47f0f0ef9956c0934ffcb5bf1
codeauslander <cesar.gusto.catano@gmail.com>
2018-08-24 20:59:59 -0500

    Finished Toolbar SideDraw components

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
+
 
 const Toolbar = props => (
   <header className='toolbar'>
     <nav className='toolbar__navigation '>
-      <div>
+      <div className='toolbar__toggle-button'>
+        <DrawerToggleButton click={props.drawerToggleClickHandler}/> 
       </div>
       <div className='toolbar__logo'><a href='/'>Logo</a></div>
       <div className='spacer_1' />
