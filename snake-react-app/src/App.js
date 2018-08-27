6ba9abe234cdfc002f70a65eaae9681726a33b69
codeauslander <cesar.gusto.catano@gmail.com>
2018-08-26 22:58:49 -0500

    Added movement to the sknake to snake-react-app

--- snake-react-app/src/App.js
+++ snake-react-app/src/App.js
@@ -5,6 +5,23 @@ import cs  from 'classnames';
 const GRID_SIZE = 35;
 const GRID = [];
 
+const TICK_RATE = 100;
+const DIRECTION = { RIGHT:'RIGHT', LEFT:'LEFT', UP:'UP', DOWN:'DOWN' };
+const DIRECTION_TICKS = {
+  RIGHT: (x,y) => ({ x: x+1, y }), 
+  LEFT: (x,y) => ({ x: x-1, y }), 
+  UP: (x,y) => ({ x, y: y-1 }),  
+  DOWN: (x,y) => ({ x, y: y+1 }), 
+}
+
+const KEY_CODES_MAPPER = {
+  37:'LEFT',
+  38:'UP',
+  39:'RIGHT',
+  40:'DOWN',
+}
+
+
 for (var i = 0; i <= GRID_SIZE; i++) {
   GRID.push(i);
 }
@@ -21,13 +38,61 @@ const getCellCs = (x, y, snake, snack) => cs('grid-cell',{
   'grid-cell-snack': isPosition(x, y, snack.coordinates.x, snack.coordinates.y),
 });
 
+const applySnakePosition = prevState => {
+  // let x;
+  // let y;
+  
+  // switch(prevState.playground.direction) {
+  //   case DIRECTION.RIGHT:
+  //       x = prevState.snake.coordinates.x + 1;
+  //       y = prevState.snake.coordinates.y;
+  //       break;
+  //   case DIRECTION.LEFT:
+  //       x = prevState.snake.coordinates.x - 1;
+  //       y = prevState.snake.coordinates.y;
+  //       break;
+  //   case DIRECTION.UP:
+  //       x = prevState.snake.coordinates.x;
+  //       y = prevState.snake.coordinates.y + 1 ;
+  //       break;
+  //   case DIRECTION.DOWN:
+  //       x = prevState.snake.coordinates.x;
+  //       y = prevState.snake.coordinates.y - 1;
+  //       break;
+  //   default:
+  //     console.log('default switch');
+  // }
 
+  // return {
+  //   snake: {
+  //     coordinates: {
+  //       x,
+  //       y,
+  //     }
+  //   }
+  // }
+
+  const directionFunction = DIRECTION_TICKS[prevState.playground.direction];
+  const coordinates = directionFunction(prevState.snake.coordinates.x, prevState.snake.coordinates.y);
+
+  return {
+    snake: {
+      coordinates,
+    }
+  }
+
+};
+
+const doChangeDirection = (direction) => () => ({playground: { direction } });
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      playground:{
+        direction: 'RIGHT',
+      },
       snake: {
         coordinates: {
           x: 20,
@@ -42,6 +107,28 @@ class App extends Component {
       }
     };
   }
+
+  componentDidMount() {
+    this.interval = setInterval(this.onTick, TICK_RATE); 
+    window.addEventListener('keyup', this.onChangeDirection, false);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    window.removeEventListener('keyup', this.onChangeDirection, false);
+  }
+
+  onTick = () => {
+    this.setState(applySnakePosition);
+  }
+
+  onChangeDirection = (event) => {
+    const direction = KEY_CODES_MAPPER[event.keyCode];
+    if (direction) {
+      this.setState(doChangeDirection(direction ));
+    }
+  }
+
   render() {
     const { snake, snack } = this.state;
     return (
@@ -58,7 +145,9 @@ const Grid = ({ snake, snack }) => (
     { GRID.map(y =>  <Row key={y} y={y} snake={snake} snack={snack} />) }
   </div>
 );
+
 const Row = ({ y, snake, snack }) => <div className='grid-row'>{GRID.map(x => <Cell key={x} x={x} y={y} snake={snake} snack={snack} />)}</div>;
+
 // const Cell = ({x,y}) => {
   // const cellClassName = cs('grid-cell',{
   //   'grid-cell-border': x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE,
