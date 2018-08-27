0d315753d5fb7640c4052b0f319e1d3100369269
codeauslander <cesar.gusto.catano@gmail.com>
2018-08-26 22:13:11 -0500

    Added style for snake and snack to snake-react-app

--- snake-react-app/src/App.js
+++ snake-react-app/src/App.js
@@ -11,10 +11,14 @@ for (var i = 0; i <= GRID_SIZE; i++) {
 
 // console.log(GRID);
 
-const isBorder = (x,y) => x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
+const isBorder = (x, y) => x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
+const isPosition = (x, y, diffX, diffY) => x === diffX && y === diffY;
 
-const getCellCs = (x,y) => cs('grid-cell',{
+
+const getCellCs = (x, y, snake, snack) => cs('grid-cell',{
   'grid-cell-border': isBorder(x,y),
+  'grid-cell-snake': isPosition(x, y, snake.coordinates.x, snake.coordinates.y),
+  'grid-cell-snack': isPosition(x, y, snack.coordinates.x, snack.coordinates.y),
 });
 
 
@@ -24,21 +28,37 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      snake: {
+        coordinates: {
+          x: 20,
+          y: 20,
+        }
+      },
+      snack: {
+        coordinates: {
+          x: 10,
+          y: 10,
+        }
+      }
     };
   }
   render() {
+    const { snake, snack } = this.state;
     return (
       <div className="app">
         <h1>Snake</h1>
-        <Grid />
+        <Grid snake={snake} snack={snack}/>
       </div>
     );
   }
 }
 
-const Grid = () => <div>{GRID.map(y =>  <Row key={y} y={y} />)}</div>;
-const Row = ({y}) => <div className='grid-row'>{GRID.map(x => <Cell key={x} x={x} y={y} />)}</div>;
+const Grid = ({ snake, snack }) => (
+  <div>
+    { GRID.map(y =>  <Row key={y} y={y} snake={snake} snack={snack} />) }
+  </div>
+);
+const Row = ({ y, snake, snack }) => <div className='grid-row'>{GRID.map(x => <Cell key={x} x={x} y={y} snake={snake} snack={snack} />)}</div>;
 // const Cell = ({x,y}) => {
   // const cellClassName = cs('grid-cell',{
   //   'grid-cell-border': x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE,
@@ -53,6 +73,6 @@ const Row = ({y}) => <div className='grid-row'>{GRID.map(x => <Cell key={x} x={x
   // return <div className={cellClassName} />;
 // };
 
-const Cell = ({x,y}) => <div className={getCellCs(x,y)} />;
+const Cell = ({ x, y, snake,snack }) => <div className={getCellCs(x, y, snake, snack)} />;
 
 export default App;
